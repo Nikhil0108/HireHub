86c1335ae64458cfc07c337e0cda41ae5f2e298a
Nikhil0108 <jindalnikhil801@gmail.com>
2024-09-20 16:01:05 +0530

    updated server.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://hiredothub.netlify.app',
+    origin:'http://hiredothub.netlify.app',
     credentials:true,
     methods:["GET","POST","PUT","DELETE"],
 }
